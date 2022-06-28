0b9b578958d0db2966aa8b046cfd829540458756
Emily Ahamed <ahamedemily7@gmail.com>
2022-06-28 14:25:37 -0400

    completed lab

--- ojectsClasses.js
+++ ojectsClasses.js
@@ -40,3 +40,103 @@ console.log(`${dessert} goes well with ${drink}`)
 //lets destructure the remaining items in the same declaration
 
 let { appetizer, entree } = meal; 
+
+console.log(`${appetizer} goes well with ${entree}`);
+
+
+//lets destructure the drink and rename it
+
+let { drink:bestDrinkEver} = meal
+console.log(bestDrinkEver)
+//console.log(drink)
+
+//looping using a for-in loop
+
+//lets loop over person object and console.log all the keys
+
+//for (key in person){
+//    console.log(key)
+//}
+
+for(key in person){
+    console.log(person[key])
+}
+
+//add properties
+
+//lets add a job key to the person object with a value of choice
+person.job = `statistical analysis and data recognition`; 
+
+//lets add pets (array) usng bracket notation 
+
+person['pets'] = ['chicken','duck']
+console.log(person)
+
+//delete properties
+
+let teams = {
+    teamOne: ['ryan', 'patrick','harry', 'haywood', 'dj'],
+    teamTwo: ['jillian', 'emily', 'kellie', 'devin', 'devin e'],
+    teamThree: ['hamzah', 'isha', 'teryn', 'rose', 'lana']
+}
+
+//delete teamTwo
+
+delete teams.teamTwo
+
+//console.log(teams)
+
+//Classes
+//lets add a greeting method/function to our class to greet the user from the dog
+class Dog {
+    constructor(name,breed,age){
+        this.name = name,
+        this.breed = breed,
+        this.age = age
+    }
+
+    greeting(){
+        console.log(`Hi, my name is ${this.name} and i am a ${this.age} year old ${this.breed})`)
+    }
+}
+
+let lassie = new Dog('lassie','collie',7)
+
+console.log(lassie)
+//console.log(lassie.breed) ---> collie
+
+let beethoven = new Dog('Beethovan', 'st.bernard',2)
+console.log(beethoven)
+
+lassie.greeting()
+beethoven.greeting()
+
+//lassie.getAge()
+//beethoven.getAge()
+
+//now we can loop over our instance of dog
+
+for (key in lassie){
+    console.log(lassie[key])
+}
+
+//extending classes
+
+class Puppy extends Dog {
+    constructor(name,breed,age,trainingLevel){
+        super(name,breed,age)
+        this.trainingLevel = trainingLevel
+    }
+
+    levelUP(num){
+        this.trainingLevel = num
+    }
+}
+
+let scrappyDoo = new Puppy('scrappy','great-dane',1,3)
+
+//console.log(scrappyDoo)
+scrappyDoo.greeting()
+scrappyDoo.levelUP(5)
+
+console.log(scrappyDoo)
